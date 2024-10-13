8dc00a3d0bb401a75e2599bfc452e80c8b05a096
RicardoAffonso0607 <ricardoraffonso@hotmail.com>
2024-10-13 11:35:31 -0300

    first commit

---
+++ src/test/java/me/dev/my_API/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.my_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
